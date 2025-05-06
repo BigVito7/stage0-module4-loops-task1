5140ac01b53531ddb8fed4ae13318b87fec65ef6
BigVito7 <biggvito7@gmail.com>
2025-05-06 10:52:09 +0300

    RepeatIsBad new

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -1,16 +1,19 @@
 package school.mjc.stage0.loops.task1;
 
 public class RepeatIsBad {
+    public static void main(String[] args) {
+        new RepeatIsBad().repeatIsBad();
+    }
 
-    @SuppressWarnings("java:S106")
-    public void repeatIsBad(){
+    public void repeatIsBad() {
         printMessage(20);
     }
-    private void printMessage(int count){
-        if(count <= 0){
+
+    private void printMessage(int count) {
+        if (count <= 0) {
             return;
         }
-        System.out.printf("writing the same code doesn't have much impact, and it's also time consuming");
-        printMessage(count-1);
+        System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        printMessage(count - 1);
     }
 }
